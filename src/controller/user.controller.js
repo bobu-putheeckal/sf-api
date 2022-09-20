dad16adedd01a8c1d07b4812cff008c918248bb8
Bobu Putheeckal <Bobu.work@gmail.com>
2022-09-20 10:05:22 -0400

    adding controller

---
+++ src/controller/user.controller.js
@@ -0,0 +1,27 @@
+const getMongoClient = require("../db/mongo.db");
+
+const getUsers = async () => {
+  const mongoClient = await getMongoClient();
+  const response = await mongoClient
+    .db("userdb")
+    .collection("users")
+    .find()
+    .project({ firstName: 1 })
+    .toArray();
+  return response;
+};
+
+const insertUser = async (user) => {
+  const mongoClient = await getMongoClient();
+  const response = await mongoClient
+    .db("userdb")
+    .collection("users")
+    .insertOne({
+      firstName: user.firstName.toString(),
+      lastName: user.lastName.toString(),
+      email: user.email.toString(),
+    });
+  return response;
+};
+
+module.exports = { getUsers, insertUser };
